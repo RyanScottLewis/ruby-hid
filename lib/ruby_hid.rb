5b374cba1cdcdda62669477f42e36447e4f6a7fa
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-10-07 21:28:06 +0100

    Added initial builds to stop unsupported event notes

--- lib/ruby_hid.rb
+++ lib/ruby_hid.rb
@@ -5,3 +5,6 @@ require_relative './ruby_hid/device.rb'
 require_relative './ruby_hid/observer.rb'
 require_relative './ruby_hid/button.rb'
 require_relative './ruby_hid/axis.rb'
+
+RubyHid::Axis.build
+RubyHid::Button.build
