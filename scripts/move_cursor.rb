7a96780c632ef7111d89c99af34c4a21a6bec495
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-10-07 21:17:22 +0100

    Added basic movement script to use joystick

--- scripts/move_cursor.rb
+++ scripts/move_cursor.rb
@@ -1,11 +1,35 @@
 require 'ostruct'
 require_relative '../lib/ruby_hid.rb'
 
-@cursor = OpenStruct.new(
+class Cursor < OpenStruct
+
+  def draw
+    puts `clear`
+    out = "+#{'-'*max_x}+\n"
+
+    max_y.times do |row|
+       out << '|'
+      max_x.times do |column|
+        if y.to_i == row and x.to_i == column
+          out << '#'
+        else
+          out << ' '
+        end
+      end
+      out << "|\n"
+    end
+
+    out << "+#{'-'*max_x}+\n"
+    puts out
+  end
+
+end
+
+@cursor = Cursor.new(
   :speed => 2,
-  :x => 50.0, :x_speed => 0.0,
-  :y => 50.0, :y_speed => 0.0,
-  :max_x => 100.0, :max_y => 100.0
+  :x => 5.0, :x_speed => 0.0,
+  :y => 5.0, :y_speed => 0.0,
+  :max_x => 40, :max_y => 15
 )
 
 axis = RubyHid::Axis.find_by_name(:left_y)
@@ -27,15 +51,15 @@ device.start_watching
 
 loop do
   if (@cursor.x >= 0 and @cursor.x_speed < 0) or
-    (@cursor.x <= @cursor.max_x and @cursor.x_speed > 0)
+    (@cursor.x <= (@cursor.max_x - 1) and @cursor.x_speed > 0)
     @cursor.x += @cursor.x_speed
   end
   if (@cursor.y >= 0 and @cursor.y_speed < 0) or
-    (@cursor.y <= @cursor.max_y and @cursor.y_speed > 0)
+    (@cursor.y <= (@cursor.max_y - 1) and @cursor.y_speed > 0)
     @cursor.y += @cursor.y_speed
   end
 
-
+  @cursor.draw
   puts "x: #{@cursor.x.to_i.to_s.ljust(4)} - y: #{@cursor.y.to_i}"
   sleep 0.1
 end
